17b0d181b866a95633093960a564fc13812f6f31
vicky-playground <90204593+vicky-playground@users.noreply.github.com>
2021-09-12 22:04:07 +0800

    Delete yabichat-master directory

--- yabichat-master/app/src/main/java/com/example/yabichat/model/Chat.kt
+++
@@ -1,8 +0,0 @@
-package com.example.yabichat.model
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Chat(val memberUID: String, val memberName: String, val lastMsg: String, val timestamp: Long = 0 ): Parcelable
-// data class Chat(val chatID: String, val memberUID: String, val lastMsg: String, val timestamp: Long = 0 ): Parcelable
--- yabichat-master/app/src/main/java/com/example/yabichat/model/Msg.kt
+++
@@ -1,7 +0,0 @@
-package com.example.yabichat.model
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class Msg(val name: String, val msg: String, val timestamp: Long = 0, val tag: String): Parcelable
